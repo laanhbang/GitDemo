0da42587b6e0980c02a8aa3d17fb8563ae3b9fad
bangla <bangla@localhost>
2015-06-10 11:12:00 +0700

    commit code 2

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -36,6 +36,7 @@ package com.toy.anagrams.lib;
  */
 final class StaticWordLibrary extends WordLibrary {
 
+    private static final String GIT = "GIT";
     private static final String[] WORD_LIST = {
         "abstraction",
         "ambiguous",
